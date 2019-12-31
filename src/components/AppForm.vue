7b68501d53f6dccad758913f8ffcf4ede04b48a3
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-12-31 01:08:03 +0100

    work on form

---
+++ src/components/AppForm.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <h2>ziga ozi</h2>
+      <!--add some things here to make it work with Netlify-->
+      <p class="visually-hidden">
+        <!--add something here to catch the bots!-->
+        <input type="hidden" name="contact" value="contact" />
+      </p>
+      <p>
+        <label>
+          Name
+          <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Email
+          <input type="text" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message
+          <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  text-transform: uppercase;
+}
+
+button {
+  width: 100%;
+}
+
+label,
+button {
+  margin-top: 20px;
+}
+</style>
