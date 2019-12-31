4f1f75e32cb6b6470af99bdf2433662821746f52
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-12-31 01:06:26 +0100

    work on about page

---
+++ src/views/About.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+    <h1>About Me</h1>
+    <!-- <p>
+      Make your own layout! You can use this if it helps:
+      <a
+        href="https://cssgrid-generator.netlify.com/"
+      >CSS Grid Generator</a>
+    </p>-->
+    <p>
+      I am a dedicated software engineer seeking a full-time position in the
+      field of Software Engineering, where I can leverage my knowledge and
+      skills for continuous improvement. I am committed to putting in my best to
+      achieve organizational goals and work effectively with others to provide
+      solutions and deliver data-driven results.
+    </p>
+    <p>
+      I am an avid learner, who is always willing to adapt, learn and unlearn
+      quickly, applying relevant knowledge along the way. I also understand that
+      great companies values teamwork and solid communication skills. This is
+      one of my greatest passions and I know without a doubt my experience in
+      this area will sustain company standards and lead the team to success.
+    </p>
+    <p>
+      I have experience in using JavaScript and the MERN stack. I adhere to
+      industry's best practices and write clean testable code, and can work
+      effectively as a collaborator using Git and Github. I am currently open to
+      hiring opportunities.
+    </p>
+    <p>
+      Some of the tools and technologies I have used include HTML5, CSS3,
+      JavaScript ES5 & ES6+, jQuery, NodeJS, React, Express JS, TypeScript,
+      MongoDB, MySQL, PostgreSQL, Bootstrap, Laravel, Travis CI, Circle CI,
+      GraphQL, Rest APIs, Jest, Git, Github, and Docker.
+    </p>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  width: 90%;
+  max-width: 800px;
+  margin: 40px auto;
+}
+
+p {
+  text-align: left;
+  font-size: 20px;
+}
+</style>
