3812c09dd244cf69c557f0f3f6a38feb98ad5736
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-12-31 01:05:09 +0100

    add in background banner

---
+++ src/components/AppMasthead.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h2>Hey there!</h2>
+    <h1>My name is {{ name }}</h1>
+    <h3 id="title">{{ title }}</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Uchenna',
+      title: 'Software Engineer | Web Developer'
+    };
+  }
+};
+</script>
